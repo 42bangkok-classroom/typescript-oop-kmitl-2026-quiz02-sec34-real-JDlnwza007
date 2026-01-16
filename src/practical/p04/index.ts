bbf9453562506462225bf44c65ee3473ca09247d
JDlnwza007 <68010147@kmitl.ac.th>
2026-01-16 20:00:47 +0700

    2

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+
 interface Address {
   street: string;
   suite: string;
@@ -9,18 +10,21 @@ interface Address {
     lng: string;
   };
 }
-interface ApiUser {
+
+interface User {
   id: number;
   name: string;
   phone: string;
   address: Address;
 }
+
 interface Todo {
   userId: number;
   id: number;
   title: string;
   completed: boolean;
 }
+
 interface UserWithTodos {
   id: number;
   name: string;
@@ -28,27 +32,34 @@ interface UserWithTodos {
   phone: string;
   todos: Todo[];
 }
-export async function getTodosByUserId(id: number): Promise<UserWithTodos | string> {
+
+export const getTodosByUserId = async (
+  id: number
+): Promise<UserWithTodos | string> => {
   try {
-    if (id < 0) return "Invalid id";
-    const usersRes = await axios.get<ApiUser[]>('https://jsonplaceholder.typicode.com/users');
-    const users = usersRes.data;
-    const todosRes = await axios.get<Todo[]>('https://jsonplaceholder.typicode.com/todos');
-    const todos = todosRes.data;
-    const foundUser = users.find((u) => u.id === id);
-    if (!foundUser) {
+    const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+    const usersUrl = "https://jsonplaceholder.typicode.com/users";
+    const todosRes = await axios.get<Todo[]>(todosUrl);
+    const usersRes = await axios.get<User[]>(usersUrl);
+
+    const user = usersRes.data.find((u) => u.id === id);
+
+    if (!user) {
       return "Invalid id";
     }
-    const userTodos = todos.filter((t) => t.userId === id);
+
+    const userTodos = todosRes.data.filter(
+      (todo) => todo.userId === id
+    );
+
     return {
-      id: foundUser.id,
-      name: foundUser.name,
-      address: foundUser.address,
-      phone: foundUser.phone,
+      id: user.id,
+      name: user.name,
+      address: user.address,
+      phone: user.phone,
       todos: userTodos,
     };
-  } catch (error) 
-  {
+  } catch {
     return "Invalid id";
   }
-}
+};
